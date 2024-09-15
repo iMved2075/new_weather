989b9fa705d9c5a1e8bb9aac74ed51404c06a2b6
ved <ved20750327@gmail.com>
2024-09-16 01:12:03 +0530

    second commit added pandas and numpy to requirements and edited weather.py to get data from the api for past 5 days and store them in hourly basis

--- weather.py
+++ weather.py
@@ -2,16 +2,32 @@ from dotenv import load_dotenv
 from pprint import pprint
 import requests
 import os
+import pandas as pd
+import numpy as np
+import datetime
 
 load_dotenv()
 
 def get_current_weather(city="Bhubaneshwar"):
+    
+    weather_data_frames = []
+    
+    for i in range(5):
+        date = (datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%Y-%m-%d")
         
-        request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
-        
+        request_url = f'http://api.weatherapi.com/v1/history.json?key={os.getenv("API_KEY")}&q={city}&dt={date}'
         weather_data = requests.get(request_url).json()
         
-        return weather_data
+        hourly_data = weather_data['forecast']['forecastday'][0]['hour']
+        
+        df = pd.DataFrame(hourly_data)
+        
+        df['date'] = date
+        
+        weather_data_frames.append(df)
+    
+    weather = pd.concat(weather_data_frames)    
+    return weather
     
 if __name__ == "__main__":
     print("\n*** Get Current Weather Conditions ***\n")
@@ -22,6 +38,8 @@ if __name__ == "__main__":
         city = "Bhubaneshwar"
         
     weather_data = get_current_weather(city)
+    
+    weather_data.to_csv("weather_data.csv", index=False)
     
     print("\n")
     pprint(weather_data)
